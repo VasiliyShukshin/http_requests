7e3a54c7bbf70eda60aae67a31d07932e2a7f9e3
Vasiliy Shukshin <vasiliy.shukshin@yandex.ru>
2022-08-11 01:32:25 +0300

    исправлено первое замечание по дз

--- q2.py
+++ q2.py
@@ -6,21 +6,31 @@ class YaUploader:
     def __init__(self, token: str):
         self.token = token
 
-    def upload(self, file_path: str):
-        url = "https://cloud-api.yandex.net/v1/disk/resources/publish"
+    def upload(self, file_path: str, file_path_on_yadisc: str, overwrite=True):
+        url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
         headers = {
             "Content-Type": "application/json",
             "Authorization": f"OAuth {self.token}"
         }
-        params = {"path": f"{file_path}"}
-        response = requests.put(url=url, headers=headers, params=params)
-        print(response.status_code)
-        return response.json()
+        params = {
+            "path": f"{file_path_on_yadisc}",
+            "overwrite": f"{overwrite}"
+        }
+        response = requests.get(url=url, headers=headers, params=params).json()
+        url2 = response["href"]
+
+        with open(file_path, 'rb') as f:
+            try:
+                response = requests.put(url=url2, files={'file': f})
+            except KeyError:
+                print(response)
+        return response.status_code
 
 
 if __name__ == '__main__':
-    path_to_file = ""
+    path_to_local_file = ""
+    file_path_on_yadisc = ""
     token = ""
     uploader = YaUploader(token)
-    result = uploader.upload(path_to_file)
+    result = uploader.upload(path_to_local_file, file_path_on_yadisc)
     pprint(result)
